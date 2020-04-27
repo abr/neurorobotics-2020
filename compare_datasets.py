5f9e5bef6427fe5a78dbe1e816b6e288d5980ddb
travis dewolf <travis.dewolf@gmail.com>
2020-04-27 15:11:16 -0400

    Update to plot images right ways up

--- compare_datasets.py
+++ compare_datasets.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from abr_analyze import DataHandler
 
 d1 = np.load('/home/tdewolf/Downloads/rover_training_0004_training_images_processed.npz')
-d2 = np.load('/home/tdewolf/Downloads/abr_analyze_training_images_processed.npz')
+d2 = np.load('/home/tdewolf/Downloads/abr_analyze_driving_training_images_processed.npz')
 
 n1 = len(d1['images'])
 n2 = len(d2['images'])
@@ -27,7 +27,7 @@ print(d1_targets)
 plt.plot(d2['targets'][:, 0], d2['targets'][:, 1], 'rx')
 plt.plot(d1['targets'][:, 0], d1['targets'][:, 1], 'bx', alpha=.5)
 plt.gca().set_aspect('equal')
-plt.savefig('plots/targets.png')
+plt.savefig('/home/tdewolf/Downloads/data/plots/targets.png')
 plt.clf()
 
 print(d1_indices)
@@ -42,14 +42,14 @@ for ii, (d1_i, d2_i) in enumerate(zip(d1_indices, d2_indices)):
     if dist1 > 3.0 or dist2 > 3.0:  # arbitrary threshold
 
         a = plt.subplot(2, 1, 1)
-        a.imshow(d1['images'][d1_i].reshape((res[0], res[1], 3)) / 255)
+        a.imshow(d1['images'][d1_i].reshape((res[0], res[1], 3)) / 255, origin='lower')
         plt.title('d1 target %i: %.3f, %.3f' % (ii, d1_targets[ii][0], d1_targets[ii][1]))
 
         b = plt.subplot(2, 1, 2)
-        b.imshow(d2['images'][d2_i].reshape((res[0], res[1], 3)) / 255)
+        b.imshow(d2['images'][d2_i].reshape((res[0], res[1], 3)) / 255, origin='lower')
         plt.title('d2 target %i: %.3f, %.3f' % (ii, d2_targets[ii][0], d2_targets[ii][1]))
 
         plt.suptitle('dist = %.3f' % max(dist1, dist2))
 
-        plt.savefig('plots/%i.png' % ii)
+        plt.savefig('/home/tdewolf/Downloads/data/plots/%i.png' % ii)
         plt.clf()
